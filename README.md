3c94cb89dfe5dc193aa47822bac1203f7370015e
redkittty <envixtypublicmail@proton.me>
2024-03-31 17:10:17 -0400

    Added more options

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wolfenstein-fortress
-A simplr text-based adventure game
+A simple text-based adventure game
+
+NOTE: This branch is meant for early-development, stuff will break
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
 fn main() {
     let error = format!("That is not an option");
-    println!("Wolfenstein Fortress v0.0.1");
+    println!("Wolfenstein Fortress v0.0.2-alpha 1");
     println!("Licensed under GNU General Public License v3.0 (GPLv3)");
+    println!("Note: The Game is currently under heavy construction");
+    println!("Some functionality might not be implemented yet");
     println!("---------------");
     // Stats
     let mut health: i8 = 100;
     let mut money: f64 = 0.00;
     let mut ammo: i64 = 0; 
-    let _stats = format!("Health = {health}, Money = {money}, Ammo = {ammo}:");
     println!("Backstory:");
     println!("You have been captured by Nazi Soldiers after your squad's surrendered");
     println!("You have been stuck in your cell for 3 days already");
@@ -62,10 +63,90 @@ fn main() {
                     println!("The new prisioners are mad at you and beat you up");
                     println!("The guards stop this and give you some medicine");
                     money += 5.00;
-                    health -= 10;
-                    health += 5;
+                    health -= 5;
                     println!("---------------");
                     println!("Health = {health}, Money = {money}, Ammo = {ammo}:");
+                    println!("---------------");
+                    println!("You go to the prision shop");
+                    println!("You see 2 items:");
+                    println!("What will you buy");
+                    println!("(1) = Paper Clip $5.00, (2) = Bread $3.00");
+                    let mut c = String::new();
+                    io::stdin()
+                        .read_line(&mut c)
+                        .expect("Failed tp read line");
+                    let c: i8 = c.trim().parse().expect("Please insert a number");
+                    if c == 1 || c == 2 {
+                        if c == 1 {
+                            money -= 5.00;
+                            println!("---------------");
+                            println!("Health = {health}, Money = {money}, Ammo = {ammo}");
+                            println!("---------------");
+                            println!("You decide to buy the Paper Clip");
+                            println!("You remember a method that the Army taught you to lock pick");
+                            println!("You are in your cell");
+                            println!("What will you do?");
+                            println!("(1) = Lock Pick, (2) = Do Nothing");
+                            let mut d = String::new();
+                            io::stdin()
+                                .read_line(&mut d)
+                                .expect("Failed to read line");
+                            let d: i8 = d.trim().parse().expect("Please insert a number");
+                            if d == 1 || d == 2 {
+                               if d == 1 {
+                                  println!("You decide to pick the lock");
+                                  println!("You eventually pick the lock");
+                                  println!("You step outside the cell");
+                                  println!("You start walking");
+                                  println!("A guard spots you");
+                                  println!("What will you do?");
+                                  println!("(1) = Kill the Guard, (2) = Return to your cell, (3) = Do Nothing");
+                                  let mut e = String::new();
+                                  io::stdin()
+                                      .read_line(&mut e)
+                                      .expect("Failed to read line");
+                                  let e: i8 = e.trim().parse().expect("Please insert a number");
+                                  if e == 1 || e == 2 || e == 3 {
+                                      if e == 1 {
+                                          println!("You decide to kill the Guard");
+                                          println!("You eventually succeed but you are badly hurt");
+                                          println!("You though get a pistol, knife, and 5 rounds");
+                                          health -= 50;
+                                          ammo += 5;
+                                          println!("---------------");
+                                          println!("Health = {health}, Money = {money}, Ammo = {ammo}");
+                                          println!("---------------");
+                                          println!("You see another guard chasing after you");
+                                          println!("What will you do?");
+                                          println!("(1) = Shoot the Guard, (2) = Stab the Guard, (3) = Surrender");
+                                          let f = String::new();
+                                          if f == 1 || f == 2 || f == 3 {
+
+                                          }
+                                          else {
+                                              println!("()", error);
+
+                                      }
+                                      if e == 2 {
+
+                                      }
+                                      if e == 3 {
+
+                                      }
+
+                                  }
+                                  else {
+                                      println!("()", error);
+                                  }
+                               }
+                            }
+                            else {
+                                println!("()", error)
+                            }
+                    }
+                    else {
+                        println!("()", error);
+
                 }
                 if b == 3 {
                     println!("You decide to attack the guard");
@@ -74,6 +155,8 @@ fn main() {
                     println!("---------------");
                     println!("GAME OVER");
                 }
+                else {
+                    println!("()", error);
             }
             else {
                 println!("{}", error);
